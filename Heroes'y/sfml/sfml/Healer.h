d62995d8508057fc686a2ccead33c45ce50df2a2
Marcin G <173072@edu.p.lodz.pl>
2015-04-23 21:52:05 +0200

    nowa klasa uzdrowicila

---
+++ Heroes'y/sfml/sfml/Healer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Unit.h"
+
+using namespace std;
+using namespace sf;
+
+class Healer : public Unit
+{
+public:
+	Healer();
+	virtual ~Healer();
+	void move();
+	virtual void Update() override;
+	void heal();
+private:
+	Image *img;
+	Texture *texture;
+	Vector2f *position = new Vector2f(60, 40);
+};
+
