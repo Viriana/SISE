f6a895f6614f9791098703416ec53407b85b0c16
Marcin G <173072@edu.p.lodz.pl>
2015-04-23 21:53:04 +0200

    heal

---
+++ Heroes'y/sfml/sfml/Healer.cpp
@@ -0,0 +1,41 @@
+#include "Healer.h"
+
+
+Healer::Healer()
+{
+	type = "Healer";
+	maxHealthPoints = 7;
+	currentHealthPoints = 7;
+	armor = 5;
+	strength = 4;
+	range = 4;
+	movementSpeed = 2;
+	setStartPosition(*position);
+	img = new Image();
+	img->loadFromFile("Data\\graphics\\battle.png");
+	img->createMaskFromColor(Color(0, 255, 255, 255));
+	texture = new Texture;
+	texture->loadFromImage(*img);
+	Renderer.SetTexture(*texture);
+	Renderer.Sprite.setTextureRect(IntRect(42, 905, 100, 120)); // left, top, width, height
+	Renderer.Sprite.setScale(1, 1);
+}
+
+
+Healer::~Healer()
+{
+	delete img;
+	delete texture;
+}
+
+void Healer::move()
+{
+}
+
+void Healer::Update()
+{
+}
+
+void Healer::heal()
+{
+}
