285f44199e6b1c5880e3acf29704ec9d869f1d6d
tuka217 <a.walasek@gazeta.pl>
2015-06-03 09:51:47 +0200

    Dodanie clipsa, poprawienie maina, zeby mozna bylo uzywac clipsa

--- Heroes'y/sfml/sfml/Main.cpp
+++ Heroes'y/sfml/sfml/Main.cpp
@@ -1,7 +1,24 @@
 #include "game.h"
+#include <tchar.h>
+#include "C:\Users\Ania\Documents\Visual Studio 2013\Projects\AplikacjaZClipsem\AplikacjaZClipsem\Projects\Source\Integration\clipscpp.h"
+#pragma comment(lib, "C:\\Users\\Ania\\Documents\\Visual Studio 2013\\Projects\\AplikacjaZClipsem\\AplikacjaZClipsem\\Projects\\Libraries\\Microsoft\\CLIPSCPP.lib")
 
-int main()
+int main(int argc, TCHAR *argv[])
 {
+	CLIPS::CLIPSCPPEnv theEnv;
+
+	// For load to work, the CLIPS file must be in the
+	// the same directory as the executable, otherwise
+	// the full path should be specified.
+	// theEnv.Load("hello.clp");
+
+	theEnv.Build("(defrule hello"
+		"   =>"
+		"  (printout t \"Hello World.\" crlf)"
+		"  (printout t \"Hit return to end.\" crlf)"
+		"  )");
+	theEnv.Reset();
+	theEnv.Run(-1);
 	Game *game = new Game();
 	game->play();
 
